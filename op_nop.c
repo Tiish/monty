b945108cabf01f3f33712cddd666841c60fd5ae2
Tiish <rickifred5@gmail.com>
2023-12-15 07:14:07 -0800

    Updated

---
+++ op_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * op_nop - does nothing
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the opcode
+ * Auth : Tiish
+ **/
+void op_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
